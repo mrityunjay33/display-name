4170ca110e14c0d9659564aad4c141b859e52afd
Mrityunjay Kumar <mk10399@gmail.com>
2023-12-26 18:12:15 +0530

    Full Name implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const App = () => {
-  
+function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [formError, setFormError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!firstName || !lastName) {
+      setFormError('Please fill in both first and last names.');
+      setFullName('');
+      return;
+    }
+
+    setFormError('');
+    setFullName(`${firstName} ${lastName}`);
+  };
 
   return (
     <div className="App">
-      <h1>Display Name</h1>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+        <br />
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+        <br />
+        <button type="submit">Submit</button>
+        {formError && <p className="error">{formError}</p>}
+      </form>
+      {fullName && <p>Full Name: {fullName}</p>}
     </div>
   );
-};
+}
 
 export default App;
